e5f9b1f0a3f492f991e93ad062d046ad9a273b92
Dian Veldsman <dianveldsman1993@gmail.com>
2019-05-26 19:43:08 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContex.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContex : DbContext
+    {
+        public DataContex(DbContextOptions<DataContex> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
